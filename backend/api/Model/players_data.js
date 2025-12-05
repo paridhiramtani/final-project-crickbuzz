464b3964c9f190c05d4d3f2d770d8a61c3d791c9
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-06 02:24:36 +0530

    Update players_data.js

--- backend/api/Model/players_data.js
+++ backend/api/Model/players_data.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const playersSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, require: true },
-  role: { type: String, require: true },
-  totalRun: { type: Number, require: true },
-  totalMatch: { type: Number, require: true },
-  photo: { type: String, require: true },
+  name: { type: String, required: true },
+  role: { type: String, required: true },
+  totalRun: { type: Number, required: true },
+  totalMatch: { type: Number, required: true },
+  photo: { type: String, required: true },
   totalWickets: { type: String },
 });
 
