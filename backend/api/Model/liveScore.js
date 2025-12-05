2bcdfccf9acaff8d89d8f0544fb2458a67318267
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-06 02:22:50 +0530

    Update liveScore.js

--- backend/api/Model/liveScore.js
+++ backend/api/Model/liveScore.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 const liveScoreSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    match : {type:String , require:true},
-    venue : {type:String , require:true},
-    team1 : {type:String , require:true},
-    score1 : {type:String , require:true},
-    team2 : {type:String , require:true},
-    score2 : {type:String , require:true},
-    toss : {type:String , require:true},
-    result : {type:String , require:true}
+    match : {type:String , required:true},
+    venue : {type:String , required:true},
+    team1 : {type:String , required:true},
+    score1 : {type:String , required:true},
+    team2 : {type:String , required:true},
+    score2 : {type:String , required:true},
+    toss : {type:String , required:true},
+    result : {type:String , required:true}
 }) 
 module.exports = mongoose.model('livescore',liveScoreSchema)
