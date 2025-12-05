c18ca240ded61201387f1dafe09825a315b234ac
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-06 02:24:07 +0530

    Update odi_rank.js

--- backend/api/Model/odi_rank.js
+++ backend/api/Model/odi_rank.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const ODI_Rank = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    position : {type:Number , require:true},
-    photo : {type:String , require:true},
-    pName : {type:String , require:true},
-    country : {type:String , require:true},
-    rating : {type:Number , require:true},
+    position : {type:Number , required:true},
+    photo : {type:String , required:true},
+    pName : {type:String , required:true},
+    country : {type:String , required:true},
+    rating : {type:Number , required:true},
 })
 
 module.exports = mongoose.model('odi',ODI_Rank)
