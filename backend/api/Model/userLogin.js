2247a0626168a8c747f10e204918aa5b598c4624
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-06 11:13:30 +0530

    Update userLogin.js

--- backend/api/Model/userLogin.js
+++ backend/api/Model/userLogin.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const userLoginSchema = new mongoose.Schema({
   // _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, require: true},
-  email: { type: String, require: true ,unique:true },
-  password: { type: String, require: true },
+  name: { type: String, required: true},
+  email: { type: String, required: true ,unique:true },
+  password: { type: String, required: true },
 });
 module.exports = mongoose.model("userLogin", userLoginSchema);
