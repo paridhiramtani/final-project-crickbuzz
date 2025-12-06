f655891e72511a1f0e976f910d00860b7d324ff8
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-06 15:23:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const http = require('http')
 const app = require('./app')
 
-const port = process.env.port
+const port = process.env.PORT || 8080; // Capital PORT is standard
 const host = process.env.host
 
 
